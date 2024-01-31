bc3fd9757bec3a2d6823799b188c9e0510a3ab71
Suraj Patil <113511921+suraj1849@users.noreply.github.com>
2024-02-01 00:18:13 +0530

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,11 +1,5 @@
 const data = [
-	{
-		id: 1,
-		name: "Agra",
-		info: "Agra is a city in northern India, known for the iconic Taj Mahal, a stunning white marble mausoleum built by Mughal emperor Shah Jahan in memory of his beloved wife. Agra is also home to several other historic and architectural wonders, including Agra Fort and Fatehpur Sikri.",
-		image: "https://cdn.thecodehelp.in/Agra.jpeg",
-		price: "35,758",
-	},
+	
 	{
 		id: 2,
 		name: "Jaipur",
