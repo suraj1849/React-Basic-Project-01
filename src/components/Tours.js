3d263bd25e79a4430ef54cf4b301b23080dcb6b1
Suraj Patil <113511921+suraj1849@users.noreply.github.com>
2024-02-02 01:11:55 +0530

    Update Tours.js

--- src/components/Tours.js
+++ src/components/Tours.js
@@ -1,4 +1,19 @@
 import Card from './Card'
 
-
+function Tours({tours, removeTour}) {
+    return (
+        <div className='container'>
+            <div>
+                <h2 className='title'> Plan With Heer Just for uh Dear Harshada</h2>
+            </div>
+            <div className='cards'>
+                {
+                    tours.map( (tour) => {
+                        return <Card key={tour.id} {...tour} removeTour={removeTour}></Card>
+                    } )
+                }
+            </div>
+        </div>
+    );
+}
 export default Tours;
